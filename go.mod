a1fcd04d4ee871591802c5359e943cfcc0b26298
Jayme Alves <jayme.neto@telefonicabigdata.onmicrosoft.com>
2023-08-07 18:11:08 -0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/sun
+
+go 1.20
---
+++ sun.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(sum(2, 2))
+}
+
+func sum(a int, b int) int {
+	return a + b
+}
+
+func times(a int, b int) int {
+	return a * b
+}
+
+func less(a int, b int) int {
+	return a - b
+}
+
+// sqp_7f39d90b361afe900d6dc7c33b4de142ac6721bb
